6af9ac1fb585491697d8c012885d201420ba29f4
C0RRIN <axelsal08@gmail.com>
2025-02-05 09:27:38 -0600

    Comenetarios en el codigo
    Se comenta el codigo para hacerlo mas legiible y entendible para el publico general

--- Web_Service/app.js
+++ Web_Service/app.js
@@ -1,12 +1,15 @@
+// Importa el módulo Express
 const express = require('express');
+// Crea una instancia de la aplicación Express
 const app = express();
 
+// Habilita el uso de JSON en las solicitudes
 app.use(express.json());
 
-
-app.get('/', (req, res) =>{
+// Define una ruta GET en la raíz que responde con "Hello World"
+app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-
+// Exporta la aplicación para ser utilizada en otro archivo
 module.exports = app;
--- Web_Service/index.js
+++ Web_Service/index.js
@@ -1,12 +1,15 @@
-const app = require('./app')
+// Importa la aplicación desde app.js
+const app = require('./app');
+// Define el puerto en el que se ejecutará el servidor
 const port = 8000;
 
+// Función asíncrona autoinvocada para iniciar el servidor
 (async () => {
-
     console.log('Starting Server...');
+    
+    // Inicia el servidor y lo escucha en el puerto definido
     app.listen(port, () => {
-        console.log(`Server is running on port ${port}`)
-    })
-
+        console.log(`Server is running on port ${port}`);
+    });
 
 })();
